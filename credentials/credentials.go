312e1880c8ee65400694108cb9268a60509dc597
Chris Johnston <chris.johnston@sport80.com>
2022-07-22 13:33:39 +0100

    Cleaned up errors in credentials

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -1,6 +1,7 @@
 package credentials
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -30,14 +31,14 @@ func GetCredentials(profile string, serialNumber string, tokenCode string) (*sts
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case sts.ErrCodeRegionDisabledException:
-				awsErr = fmt.Errorf(sts.ErrCodeRegionDisabledException, aerr.Error())
+				awsErr = errors.New(fmt.Sprint(sts.ErrCodeRegionDisabledException, aerr.Error()))
 			default:
-				awsErr = fmt.Errorf(aerr.Error())
+				awsErr = errors.New(aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			awsErr = fmt.Errorf(err.Error())
+			awsErr = errors.New(err.Error())
 		}
 
 		return nil, awsErr
